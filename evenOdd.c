9f6924b320a1001f41589e669e2a08bc398d2f0f
PauloHemmel <paulogustavohemmel@gmail.com>
2023-08-29 16:14:17 -0400

    correction

--- evenOdd.c
+++ evenOdd.c
@@ -4,15 +4,14 @@ void evenOdd(int n)
     if(n%2==0)
         printf("%d is Even", n);
     else
-        printf("%d is Odd", n);
-    
+        printf("%d is Odd", n);  
 }
 
 int main ()
 {
     int x;
     printf("Enter a Number : ");
-    scanf("%d, &x");
+    scanf("%d",&x);
     evenOdd(x);
     return 0;
 
