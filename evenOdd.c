0c91acca0d2c533dba41f74acf854f4b97b3a0cf
PauloHemmel <paulogustavohemmel@gmail.com>
2023-08-29 16:10:02 -0400

    add cod

--- evenOdd.c
+++ evenOdd.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
-
+void evenOdd(int n)
+{
+    if(n%2==0)
+        printf("%d is Even", n);
+    else
+        printf("%d is Odd", n);
+    
+}
 
 int main ()
 {
-    int x
+    int x;
     printf("Enter a Number : ");
-    
+    scanf("%d, &x");
+    evenOdd(x);
+    return 0;
+
 }
